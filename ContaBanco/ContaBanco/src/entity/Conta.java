72c0ee70e141e398aa4adcbb589f719581489560
Matheus Torres <60990557+torresdantas@users.noreply.github.com>
2025-03-13 12:40:55 -0300

    Update Conta.java - Classe exibirDados

--- ContaBanco/ContaBanco/src/entity/Conta.java
+++ ContaBanco/ContaBanco/src/entity/Conta.java
@@ -1,9 +1,14 @@
 package entity;
 
 public class Conta {
-    public int numero;
+    public int numeroConta;
     public String agencia;
     public String nomeCliente;
     public double saldo;
 
+
+    public String exibirDados() {
+        return "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco!\n" +
+               "Sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo é R$ " + saldo;
+    }
 }
